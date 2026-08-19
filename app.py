ce76611b535c4eb975bd31d693a8101304e5bbc4
prasetyiandika29-prog <prasetyiandika29@gmail.com>
2026-08-19 16:04:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ st.set_page_config(
     layout="centered"
 )
 
+# MASUKKAN NOMOR WHATSAPP KAMU DI SINI (Gunakan kode negara 62, contoh: 628123456789)
+NOMOR_WA = "62895420464292" 
+
 # Kustomisasi CSS untuk Desain Romantis
 st.markdown("""
     <style>
